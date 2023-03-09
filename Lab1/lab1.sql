3d2eed2103c536099988a1584b7e03949d2d3422
nibibee <72893410+nibitoff@users.noreply.github.com>
2023-03-09 16:43:31 +0300

    Create lab1.sql

---
+++ Lab1/lab1.sql
@@ -0,0 +1,32 @@
+do
+$$
+DECLARE
+    given_schema text;--varchar(7);
+    countTables integer;
+    countIndexes integer;
+    countColumns integer;
+    row record;
+begin
+    given_schema = 'public';
+    select count(*) from information_schema.tables where table_schema = given_schema into countTables;
+    select count(*) from information_schema.columns where table_schema = given_schema into countColumns;
+    select count(*) from pg_indexes where schemaname = given_schema into countIndexes;
+
+    raise info 'Количество таблиц в схеме % - %', given_schema, countTables;
+    raise info 'Количество столбцов в схеме % - %',given_schema, countColumns;
+    raise info 'Количество индексов в схеме % - %',given_schema, countIndexes;
+
+    raise info  'Таблицы схемы %', given_schema;
+    raise info 'Имя                         Столбцов           Строк';
+    raise info '--------------------------------------------------------------------------';
+    FOR row in select information_schema.columns.table_name AS "Имя", count(information_schema.columns.column_name) as "Столбцов", pg_stat_user_tables.n_live_tup as "Строк"
+    from information_schema.columns
+    inner join pg_stat_user_tables on information_schema.columns.table_name = pg_stat_user_tables.relname
+    where information_schema.columns.table_schema = given_schema
+    group by information_schema.columns.table_name, pg_stat_user_tables.n_live_tup
+    LOOP
+        RAISE INFO '% % %', rpad(row."Имя", 30, ' '), rpad(row."Столбцов"::text, 15, ' '), rpad(row."Строк"::text, 15, ' ');
+    END LOOP;
+end;
+$$
+LANGUAGE plpgsql;
